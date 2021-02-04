051568973d7b6bb63b60fbaf4356be9efca2723a
youngjuning <youngjuning@aliyun.com>
2021-02-04 14:32:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,11 +19,10 @@ fi
 
 release_uri="$1/download/$2-${target}.zip"
 
-home_dir =
 if [ ! $3 ]; then
-  echo ".$1"
+  home_dir=".$1"
 else
-  echo $3
+  home_dir="$3"
 fi
 echo ${home_dir}
 install_home="${install_home:-$HOME/$home_dir}"
